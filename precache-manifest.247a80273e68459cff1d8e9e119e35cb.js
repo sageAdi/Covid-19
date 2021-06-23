1c9182685a468cd9dce8815fc7f2193d72a8480b
sageAdi <adityamaurya100@gmail.com>
2021-06-23 21:19:59 +0530

    Updates

---
+++ precache-manifest.247a80273e68459cff1d8e9e119e35cb.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2895f178658ef40e6174ef650b85fa4",
+    "url": "/Covid-19/index.html"
+  },
+  {
+    "revision": "c4cbe9ff09c32a38a0e4",
+    "url": "/Covid-19/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "4f6cfdbe50370a55bd4b",
+    "url": "/Covid-19/static/css/main.71d6c716.chunk.css"
+  },
+  {
+    "revision": "c4cbe9ff09c32a38a0e4",
+    "url": "/Covid-19/static/js/2.1321e774.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Covid-19/static/js/2.1321e774.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f6cfdbe50370a55bd4b",
+    "url": "/Covid-19/static/js/main.9630de95.chunk.js"
+  },
+  {
+    "revision": "eaf06e2f1275a620e677",
+    "url": "/Covid-19/static/js/runtime-main.ee81733d.js"
+  },
+  {
+    "revision": "665c3f84472ccb125229df74fab1fc70",
+    "url": "/Covid-19/static/media/About_pic.665c3f84.png"
+  },
+  {
+    "revision": "cb40d0c8d2ec7afde74554a0a85a6a20",
+    "url": "/Covid-19/static/media/contaminated-objects.cb40d0c8.svg"
+  },
+  {
+    "revision": "bcf54684a30f20054fe2f5416da5db70",
+    "url": "/Covid-19/static/media/cough.bcf54684.png"
+  },
+  {
+    "revision": "47c162f95318f7678d2ad4676e460efc",
+    "url": "/Covid-19/static/media/covid_logo-copy.47c162f9.png"
+  },
+  {
+    "revision": "0abc487f124794c529c00b0ab036f9f2",
+    "url": "/Covid-19/static/media/human-contact.0abc487f.svg"
+  },
+  {
+    "revision": "5cd105918ce3e7eaf54a74aa2a2d3d84",
+    "url": "/Covid-19/static/media/social-gathering.5cd10591.svg"
+  }
+]);
